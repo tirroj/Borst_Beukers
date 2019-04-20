3f0eae2d0b7c36cc424680ab3688b171d507046f
Robbin <robbin-b@live.nl>
2019-04-20 12:16:59 +0200

    -

---
+++ src/jabberpoint/colorchooser/ColorChooser.java
@@ -0,0 +1,26 @@
+package jabberpoint.colorchooser;
+
+import javax.swing.*;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
+
+public class ColorChooser extends JColorChooser {
+    private AbstractColorChooserPanel colorPanel;
+    private JDialog dialog;
+
+    public ColorChooser() {
+        AbstractColorChooserPanel[] panels = getChooserPanels();
+        for (AbstractColorChooserPanel accp : panels) {
+            if (accp.getDisplayName().equals("Swatches")) {
+                colorPanel = accp;
+                break;
+            }
+        }
+        JOptionPane optionPane = new JOptionPane();
+        optionPane.setMessage(new Object[] {"Select the color: ", colorPanel});
+        dialog = optionPane.createDialog(colorPanel, "Color selector");
+    }
+    public void showDialog() {
+        dialog.setVisible(true);
+    }
+}
+
---
+++ src/jabberpoint/widthChooser/LineWidthSlider.java
@@ -0,0 +1,29 @@
+package jabberpoint.widthChooser;
+
+import javax.swing.JSlider;
+import javax.swing.JOptionPane;
+import javax.swing.JDialog;
+
+public class LineWidthSlider {
+    private JSlider slider;
+    private JDialog dialog;
+
+    public LineWidthSlider() {
+        slider = new JSlider(JSlider.HORIZONTAL, 0, 30, 3);
+        slider.setMinorTickSpacing(1);
+        slider.setMajorTickSpacing(3);
+        slider.setPaintTicks(true);
+        slider.setPaintLabels(true);
+        slider.setLabelTable(slider.createStandardLabels(3));
+        JOptionPane optionPane = new JOptionPane();
+        optionPane.setMessage(new Object[] {"Select the width: ", slider});
+        dialog = optionPane.createDialog(slider, "Line width selector");
+
+    }
+    public void showDialog() {
+        dialog.setVisible(true);
+    }
+    public int getWidth() {
+        return slider.getValue();
+    }
+}
